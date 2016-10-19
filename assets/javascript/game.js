45c178dd4f420cf6b4dc0913bb02e705de47fcac
mmeder2016 <mmeder2016@gmail.com>
2016-10-19 15:43:15 -0500

    Testing and making changes to make game work as one would expect.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,41 @@
-// GLOBAL VARAIABLES
-
-function TestJ()
-{
-	document.getElementById("id-button").value = "Tested.";
-}
-
+// GLOBAL Functions
 document.onkeyup = function(event) {
 	if(gameObj.gameInProgress())
 	{
 		gameObj.addGuess(String.fromCharCode(event.keyCode).toUpperCase());
-		console.log("document.onkeyup: " + String.fromCharCode(event.keyCode).toUpperCase());
 	}
 	else
 	{
 		// Start a new word and reset variables
-		gameObj.resetGame();
-		console.log("document.onkeyup: new word: " + gameObj.wordToGuess);
+		gameObj.startGame();
 	}
 };
 
+updateHTML = function()
+{
+	var formatPlayersWord = "";
+	if(gameObj.gameInProgress())
+	{
+		// When playing the game, format the playersWord for html with a 
+		// space between the letters - it just looks nicer
+		for (var i = 0; i < gameObj.playersWord.length; i++) {
+			formatPlayersWord += gameObj.playersWord.charAt(i);
+			if(i + 1 != gameObj.playersWord.length) {
+				formatPlayersWord += " ";
+			}
+		}
+	}
+	else 
+	{
+		formatPlayersWord = "NONE";
+	}
+	document.getElementById("players-word").innerHTML = formatPlayersWord;
+	document.getElementById("used-letters").innerHTML = gameObj.usedLetters;
+	document.getElementById("num-wins").innerHTML = gameObj.winCount;
+	document.getElementById("guesses-remaining").innerHTML = gameObj.guessesRemaining;
+}
+
+// Game Object
 var gameObj = {
 	// This will hold the current word to guess
 	wordToGuess: "", 
@@ -33,6 +50,8 @@ var gameObj = {
 	// Flag to indicate if we are in the middle of a game
 	gameHasStarted:false,
 
+	guessesRemaining: 10,
+
 	words3: [
 		'VANILLA',
 		'CHOCOLATE',
@@ -43,10 +62,20 @@ var gameObj = {
 		return this.gameHasStarted;
 	},
 	resetGame: function()
+		{
+		this.wordToGuess = "";
+		this.usedLetters = "";
+		this.playersWord = "NONE";
+		this.guessesRemaining = 0;
+		this.gameHasStarted = false;
+	},
+	startGame: function()
 	{
 		this.wordToGuess = "";
 		this.usedLetters = "";
+		this.playersWord = "";
 		this.gameHasStarted = true;
+		this.guessesRemaining = 10;
 		this.setNewWord();
 	},
 	setNewWord: function()
@@ -56,24 +85,34 @@ var gameObj = {
 		for (var i = 0; i < this.wordToGuess.length; ++i) {
 		    this.playersWord += '_';
 		}
-		this.updateHTML();	
+		updateHTML();	
 	},
 	addGuess: function(letter)
 	{
 		// if the letter has not been guessed already
 		if(this.usedLetters.indexOf(letter) == -1)
 		{
+			this.guessesRemaining--; 
 			this.usedLetters += letter;
-			for (var i = 0; i <= this.wordToGuess.length; i++) {
+			// Check all the letters in var "wordToGuess"
+			for (var i = 0; i < this.wordToGuess.length; i++) {
+				// If we find the letter in var "wordToGuess"
+				// Remember, all unguessed letters in var "playersWord" are 
+				// masked with an underscore
 				if(letter === this.wordToGuess.charAt(i) ) {
-			    	// Replace the _ with the letter
+			    	// Replace the _ with the letter by creating a new string
 			    	this.playersWord = this.playersWord.substring(0, i) + letter + this.playersWord.substring(i+1);
 			    }
 			}			
 			if(this.wordToGuess === this.playersWord) {
 				this.win();
 			}
-			this.updateHTML();
+			else if(this.guessesRemaining === 0)
+			{
+				alert("You're out of guesses fool! The word was " + this.wordToGuess);
+				this.resetGame();
+			}
+			updateHTML();
 		}
 		else {
 			alert("Letter " + letter + " has already been guessed!");
@@ -82,13 +121,6 @@ var gameObj = {
 	win: function()
 	{
 		this.winCount++;
-		this.gameHasStarted = false;
+		this.resetGame();
 	},
-	updateHTML: function()
-	{
-		document.getElementById("players-word").innerHTML = this.playersWord;
-		document.getElementById("used-letters").innerHTML = this.usedLetters;
-		document.getElementById("num-wins").innerHTML = this.winCount;
-	}
 }
-
